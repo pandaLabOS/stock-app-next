7f040d243ec08fa3f10680e4f6720f4baab649c2
pandaLabOS <u6215112@au.edu>
2023-02-03 20:56:51 +0100

    Added "About Page"

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>About</title>
+      </Head>
+      <h1>About Page</h1>
+      <p>About Ornwara</p>
+    </>
+  )
+}
