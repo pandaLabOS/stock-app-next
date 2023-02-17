b0bacb68789fb0ec8f343a12fcedaeef2e4e9475
pandaLabOS <u6215112@au.edu>
2023-02-17 19:23:11 +0700

    Updated products/index.js

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -8,22 +8,20 @@ export default async function handler(req, res) {
     if (req.method === 'GET') {
         const docs = await Product.find()
         res.status(200).json(docs)
+
     } else if (req.method === 'POST') {
-        // console.log(req.body)
-        // res.status(200).json(req.body)
-        
-        const doc = await Article.create(req.body)
+        const doc = await Product.create(req.body)
         res.status(201).json(doc)
+
     } else {
         res.setHeader('Allow', ['GET', 'POST'])
         res.status(405).end(`Method ${req.method} Not Allowed`)
     }
 }
     const productSchema = new Schema({
-        title: String,
-        content: String
+        code: String,
+        name: String,
+        price: Number
     })
 
     const Product = models?.product || model('product', productSchema);
-    //if NextJS already uses mongoose and it is already defined, skip the new model creation (models?.Article) = check
-    //otherwise, create a new model (model('article', articleSchema))
