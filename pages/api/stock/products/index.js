d6bdadd6bd2cf86a295019a5ada8f92a3deb242b
pandaLabOS <u6215112@au.edu>
2023-02-22 20:24:26 +0700

    Changed connectionString

--- pages/api/stock/products/index.js
+++ pages/api/stock/products/index.js
@@ -1,5 +1,5 @@
 import {connect, model, models, Schema} from "mongoose"
-const connectionString = process.env.MONGODB_URI
+const connectionString = "mongodb+srv://user1:VHh5rZeupiNstAJQ@stockappnext.melcos9.mongodb.net/stock"
 
 export default async function handler(req, res) {
     await connect(connectionString);
